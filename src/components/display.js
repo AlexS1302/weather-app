a1a864061de8bffdb681b0739c129c14eb5521db
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-04-25 18:03:06 +0100

    Implement setUnitGroup()

--- src/components/display.js
+++ src/components/display.js
@@ -1,9 +1,13 @@
 import { changeUnit, changeLocation, fetchWeather } from "./fetchWeather";
 
-
-function getLocation() {
+function setLocation() {
     const searchBar = document.getElementById("search");
     const searchString = searchBar.value;
     searchBar.value = "";
     changeLocation(searchString);
+}
+
+function setUnitGroup() {
+    const unit = document.getElementById("units")
+    changeUnit(unit.value);
 }
