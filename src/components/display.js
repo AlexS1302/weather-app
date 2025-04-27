e716b647ffcee38d4551c8b1f4cab490357d0405
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-04-27 18:22:15 +0100

    Add updateTodayForecast()
    - Update today's weather data, including location, date, weather icon,
    temperature and weather condition

--- src/components/display.js
+++ src/components/display.js
@@ -1,5 +1,48 @@
 import { changeUnit, changeLocation, fetchWeather } from "./fetchWeather";
 
+async function updateWeatherInfo() {
+    try {
+        const weatherData = await fetchWeather();
+        updateTodayForecast(weatherData);
+
+    } catch (e) {
+        console.log("Failed to update weather info:", e);
+    }
+}
+
+async function updateTodayForecast(weatherData) {
+
+    // Left card section
+    const location = document.querySelector(".location");
+    location.textContent = weatherData.general.address;
+
+    const date = document.querySelector(".date");
+    date.textContent = weatherData.general.date;
+
+    const weatherIcon = document.querySelector(".weather-icon");
+    const iconPath = await setWeatherIcon(weatherData.current.weatherIcon);
+    weatherIcon.src = iconPath;
+
+    const temperature = document.querySelector(".temperature");
+    temperature.textContent = weatherData.current.temperature;
+
+    const weatherDescription = document.querySelector(".weather-desc");
+    weatherDescription.textContent = weatherData.current.condition;
+
+    // Right card section
+    
+}
+
+
+// Event Listeners
+const searchBtn = document.getElementById("search-btn");
+searchBtn.addEventListener("click", (event) => {
+    event.preventDefault;
+    setLocation();
+    updateWeatherInfo();
+});
+
+// Helper Functions
 function setLocation() {
     const searchBar = document.getElementById("search");
     const searchString = searchBar.value;
@@ -12,27 +55,9 @@ function setUnitGroup() {
     changeUnit(unit.value);
 }
 
-async function updateWeatherInfo() {
-    try {
-        const weatherData = await fetchWeather();
-
-        const location = document.querySelector(".location");
-        location.textContent = weatherData.general.address;
-
-        const date = document.querySelector(".date");
-        date.textContent = weatherData.general.date;
-
-    } catch (e) {
-        console.log("Failed to update weather info:", e);
-    }
+async function setWeatherIcon(iconName) {
+    const iconPromise = await import(`../assets/icons/${iconName}.svg`);
+    return iconPromise.default;
 }
-
-// Event Listeners
-const searchBtn = document.getElementById("search-btn");
-searchBtn.addEventListener("click", (event) => {
-    event.preventDefault;
-    setLocation();
-    updateWeatherInfo();
-});
 
 export {updateWeatherInfo};
