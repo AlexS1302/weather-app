3492d7402a00712cd146fc71057f550f4790e75c
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-04-30 14:12:14 +0100

    Add corresponding units to wind speed

--- src/components/updateDisplay.js
+++ src/components/updateDisplay.js
@@ -1,11 +1,12 @@
 import { changeUnit, changeLocation, fetchWeather } from "./fetchWeather";
-import { updateTodayForecast, updateWeeklyForecast } from "./dynamicUI";
+import { updateElement, updateTodayForecast, updateWeeklyForecast } from "./dynamicUI";
 
 async function updateWeatherInfo() {
     try {
         const weatherData = await fetchWeather();
         updateTodayForecast(weatherData);
         updateWeeklyForecast(weatherData);
+        setUnitGroup();
 
     } catch (e) {
         console.error("Failed to update weather info:", e);
@@ -35,9 +36,24 @@ function setLocation() {
 }
 
 function setUnitGroup() {
-    const unit = document.getElementById("units")
-    changeUnit(unit.value);
+    const unit = document.getElementById("units").value;
+    changeUnit(unit);
+    
+    let unitLabel = "";
+    switch (unit) {
+        case "uk":
+            unitLabel = "km/h";
+            break;
+        case "metric":
+            unitLabel = "m/s";
+            break;
+        case "us":
+            unitLabel = "mph";
+            break;
+        case "base":
+            unitLabel = "units";
+            break;
+    }
+    updateElement(".wind-speed-unit", "textContent", ` ${unitLabel}`);
 }
-
-
 export {updateWeatherInfo};
